01e5258bd22ab6c4517130e3962fc091732698d7
erwinel <leonard.t.erwine.ctr@mail.mil>
2020-02-24 15:09:37 -0500

    Moved non-typings files to their own projects

--- ServerScoped/base.d.ts
+++ ServerScoped/base.d.ts
@@ -6961,6 +6961,24 @@ declare interface Id {
     type: "AbstractAjaxProcessor";
 }
 
+declare interface ObjectConstructor {
+    extendsObject<B, E extends Omit<ICustomClassPrototype0<any, any, string>, "initialize">, P extends Omit<B, "type"> & E>(base: B, proto: E): P;
+}
+
+declare interface IAbstractAjaxProcessor {
+    CALLABLE_PREFIX: 'ajaxFunction_';
+    process(): any;
+    newItem(name: string): XMLNode;
+    getParameter(name: string): $$rhino.String;
+    getDocument(): XMLDocument2;
+    getRootElement(): XMLNode;
+    getName(): $$rhino.String;
+    getType(): $$rhino.String;
+    getChars(): $$rhino.String;
+    setAnswer(value: any): void;
+    setError(error: any): void;
+    type: "AbstractAjaxProcessor";
+}
 declare interface AbstractAjaxProcessorConstructor {
     new(value?: any): IAbstractAjaxProcessor;
     (): any;
--- ServerScoped/x_44813_usasoc_cst/globals.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../base.d.ts" />
-
-declare namespace x_44813_usasoc_cst {
-    export const global: ScopedGlobals;
-}
